521564074d57994868b537299cb2599216b98772
SyntaxTechnologies <instructor@syntaxtechs.com>
2019-12-22 12:07:31 -0500

    Locators Demo class added

--- src/com/class01/LunchChromeBrowser.java
+++ src/com/class01/LunchChromeBrowser.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LunchChromeBrowser {
-
+	public static String path="";
 	public static void main(String[] args) throws InterruptedException {
 
-	
+	String path="";
 		//					Key							Value
 		System.setProperty("webdriver.chrome.driver", "driversf/chromedriver");
 		WebDriver driver=new ChromeDriver();
